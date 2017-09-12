b0b93d667ee904a91570bd713e4b19b51d389f8c
Norraniti-Yotha <31031648+Norraniti-Yotha@users.noreply.github.com>
2017-09-12 16:02:02 +0700

    ใช้เครื่องหมายถูกต้อง

---
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPE200Lab1
+{
+    class RPNCalculatorEngine : CalculatorEngine
+    {
+        public string Process(string str)
+        {
+            List<string> parts = str.Split(' ').ToList<string>();
+            string result = "0";
+            Stack rpnStack = new Stack();
+            for (int i = 0; parts.Count > i;i++)
+            {
+                if (isNumber(parts[i]))
+                {
+                    rpnStack.Push(parts[i]);
+                }
+                if (isOperator(parts[i]))
+                {
+                    String second = rpnStack.Pop().ToString();
+                    String frist = rpnStack.Pop().ToString();
+                    result = calculate(parts[i], frist, second,4);
+                    rpnStack.Push(result);
+                    
+                }
+            }
+            return result;
+        }
+    }
+}
